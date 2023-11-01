e29a61a6acc2c552a85a708a4123ba2f0d958974
Christoph Richter <crichter@cocus.com>
2023-11-01 15:07:43 +0100

    switched to terraform cloud to store states

--- test-git-auto/main.tf
+++ test-git-auto/main.tf
@@ -1,5 +1,4 @@
 provider "aws" {
-  profile = "default"
   region  = "us-east-1"
 }
 
@@ -16,20 +15,18 @@ terraform {
       name = "ACloudGuru-AWS-Deployment"
     }
   }
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.5.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.6.0"
-    }
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.7.0"
-    }
-  }
-  required_version = ">= 0.13"
+  # required_providers {
+  #   aws = {
+  #     source  = "hashicorp/aws"
+  #     version = ">= 5.5.0"
+  #   }
+  #   helm = {
+  #     source  = "hashicorp/helm"
+  #     version = ">= 2.6.0"
+  #   }
+  #   kubectl = {
+  #     source  = "gavinbunney/kubectl"
+  #     version = ">= 1.7.0"
+  #   }
+  # }
 }
