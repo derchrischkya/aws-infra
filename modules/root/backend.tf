ef126faa4c97dd28f2aaa31b0dd9ca25b61da405
Christoph Richter <crichter@cocus.com>
2023-11-01 13:13:04 +0100

    switched to terraform cloud to store states

---
+++ modules/root/backend.tf
@@ -0,0 +1,10 @@
+
+//Do not uncomment this file. This file will be uncommented only in Github Actions
+# terraform {
+#   backend "remote" {
+#     organization = "derchrischkya"
+#     workspaces {
+#       name = "ACloudGuru-AWS-Deployment"
+#     }
+#   }
+# }
