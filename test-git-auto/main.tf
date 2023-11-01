ebca7d36fc3f546b53573a4808d044f8528f4a8f
Christoph Richter <crichter@cocus.com>
2023-11-01 14:27:26 +0100

    switched to terraform cloud to store states

--- test-git-auto/main.tf
+++ test-git-auto/main.tf
@@ -7,3 +7,29 @@ resource "aws_instance" "example" {
    ami           = "ami-2757f631"
    instance_type = "t2.micro"
  }
+
+terraform {
+//Do not uncomment this file. This file will be uncommented only in Github Actions
+  backend "remote" {
+    organization = "derchrischkya"
+    workspaces {
+      name = "ACloudGuru-AWS-Deployment"
+    }
+  }
+  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.5.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.6.0"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
+    }
+  }
+  required_version = ">= 0.13"
+}
