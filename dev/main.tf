9ee18b9e4037689ccdb3948232facd4f1d1bd3d1
Christoph Richter <crichter@cocus.com>
2023-11-01 16:12:58 +0100

    switched to terraform cloud to store states

--- dev/main.tf
+++ dev/main.tf
@@ -1,15 +1,3 @@
-terraform {
-  backend "remote" {
-    # The name of your Terraform Cloud organization.
-    organization = "derchrischkya"
-
-    # The name of the Terraform Cloud workspace to store Terraform state files in.
-    workspaces {
-      name = "ACloudGuru-AWS-Deployment"
-    }
-  }
-}
-
 provider "aws" {
   region  = "us-east-1"
 }
