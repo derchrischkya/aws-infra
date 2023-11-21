525ee105919b8b3f494a0c55245d71703cc15814
Christoph Richter <christoph.richter1997@gmail.com>
2023-11-22 00:15:26 +0100

    added grafana monitoring

---
+++ local/main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region  = "us-east-1"
+  profile = "acloudguru-dev"
+}
+
+module "root-module" {
+  source = "../modules/root"
+  ### VPC ### 
+  vpc_name           = "eks-terraform-vpc"
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  ### Cluster ##
+  cluster_name                    = "eks-terraform-project"
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+
+  ### Grafana ###
+  prometheus_password = var.prometheus_password
+  prometheus_username = var.prometheus_username
+  loki_password       = var.loki_password
+  loki_username       = var.loki_username
+  #prometheus_url = 
+  #loki_url = 
+}
--- local/vars.tf
+++ local/vars.tf
@@ -1,19 +1,23 @@
 variable "prometheus_username" {
   type        = string
   description = "The username for Prometheus"
+  default     = ""
 }
 
 variable "prometheus_password" {
   type        = string
   description = "The password for Prometheus"
+  default     = ""
 }
 
 variable "loki_username" {
   type        = string
   description = "The username for Loki"
+  default     = ""
 }
 
 variable "loki_password" {
   type        = string
   description = "The password for Loki"
+  default     = ""
 }
