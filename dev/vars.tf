8f1b4e03c898c5663e1f27ea540998a1002073cc
Christoph Richter <christoph.richter1997@gmail.com>
2023-11-21 22:49:52 +0100

    added grafana monitoring

---
+++ dev/vars.tf
@@ -0,0 +1,19 @@
+variable "prometheus_username" {
+  type        = string
+  description = "The username for Prometheus"
+}
+
+variable "prometheus_password" {
+  type        = string
+  description = "The password for Prometheus"
+}
+
+variable "loki_username" {
+  type        = string
+  description = "The username for Loki"
+}
+
+variable "loki_password" {
+  type        = string
+  description = "The password for Loki"
+}
--- modules/root/provider.tf
+++ modules/root/provider.tf
@@ -17,14 +17,15 @@ terraform {
   required_version = ">= 0.13"
 }
 
+data "aws_eks_cluster_auth" "cluster-auth" {
+  depends_on = [module.eks.cluster_name]
+  name       = module.eks.cluster_name
+}
+
 provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-      command     = "aws"
-    }
-}
+    token                  = data.aws_eks_cluster_auth.cluster-auth.token
+  }
 }
