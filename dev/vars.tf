3032b82abe988077d8e9604124577c63573280e9
Christoph Richter <christoph.richter1997@gmail.com>
2023-11-21 23:48:02 +0100

    added grafana monitoring

--- dev/vars.tf
+++ dev/vars.tf
@@ -1,21 +1,25 @@
 variable "prometheus_username" {
   type        = string
   description = "The username for Prometheus"
+  default     = ""
 }
 
 variable "prometheus_password" {
   type        = string
   description = "The password for Prometheus"
+  default     = ""
 }
 
 variable "loki_username" {
   type        = string
   description = "The username for Loki"
+  default     = ""
 }
 
 variable "loki_password" {
   type        = string
   description = "The password for Loki"
+  default     = ""
 }
 
 variable "AWS_ACCESS_KEY_ID" {}
