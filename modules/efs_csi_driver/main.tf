92eb19fea27b88a438be14bfe3055f61cbd2e866
Christoph Richter <christoph.richter1997@gmail.com>
2023-11-24 00:52:33 +0100

    added csi_efs_driver helm chart to repo

--- modules/efs_csi_driver/main.tf
+++ modules/efs_csi_driver/main.tf
@@ -9,4 +9,17 @@ resource "helm_release" "efs_csi_driver" {
     name = "controller.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
     value = var.service_account_role_arn
   }
+
+  set {
+    name = "storageClasses[0]"
+    value = jsonencode({
+      "name" : "efs-sc",
+      "parameters" : {
+        "fileSystemId" : var.efs_id,
+        "directoryPerms" : "777",
+        "path" : "/",
+        "provisionerName" : "efs.csi.aws.com"
+      }
+    })
+  }
 }
