17bd4c099a59d89a47604de97b17b759c1d74d24
Christoph Richter <christoph.richter1997@gmail.com>
2023-11-23 23:32:22 +0100

    added csi_efs_driver helm chart to repo

--- modules/efs_csi_driver/main.tf
+++ modules/efs_csi_driver/main.tf
@@ -1,21 +1,21 @@
 resource "helm_release" "efs_csi_driver" {
-  name       = "efs_csi_driver"
-  repository = "https://kubernetes-sigs.github.io/aws-efs-csi-driver/"
+  name       = "efs-csi-driver"
+  repository = "https://kubernetes-sigs.github.io/aws-efs-csi-driver"
   chart      = "aws-efs-csi-driver"
   namespace  = "kube-system"
 
-  set {
-    name = "controller.serviceAccount.name"
-    value = "efs-csi-controller-sa"
-  }
+  # set {
+  #   name    = "controller.serviceAccount.name"
+  #   value   = "efs-csi-controller-sa"
+  # }
 
-  set {
-    name = "controller.serviceAccount.create"
-    value = "true"
-  }
+  # set {
+  #   name = "controller.serviceAccount.create"
+  #   value = "true"
+  # }
 
-  set {
-    name = "controller.serviceAccount.annotations.eks.amazonaws.com/role-arn"
-    value = var.service_account_role_arn
-  }
+  # set {
+  #   name = "controller.serviceAccount.annotations.eks.amazonaws.com/role-arn"
+  #   value = var.service_account_role_arn
+  # }
 }
